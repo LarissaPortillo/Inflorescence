b120cd81312c7ab0f814065c45f8e6ca6f011f70
Larissa Portillo <larissaportillo10@gmail.com>
2022-08-11 09:51:12 -0400

    https instead of http on imported font link

--- views/Cart.jsx
+++ views/Cart.jsx
@@ -8,7 +8,7 @@ class Cart extends React.Component{
                 <meta charSet="UTF-8" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link href="http://fonts.cdnfonts.com/css/drugs" rel="stylesheet"></link>
+                <link href="https://fonts.cdnfonts.com/css/drugs" rel="stylesheet"></link>
                 <title>Cart</title>
             </head>
             <body>
--- views/Home.jsx
+++ views/Home.jsx
@@ -9,7 +9,7 @@ class Home extends React.Component{
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link rel="stylesheet" href="/css/home.css"></link>
-                <link href="http://fonts.cdnfonts.com/css/drugs" rel="stylesheet"></link>
+                <link href="https://fonts.cdnfonts.com/css/drugs" rel="stylesheet"></link>
                 <title>Inflorescence</title>
             </head>
             <body>
